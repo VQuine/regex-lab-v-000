ac63d44b6f4a463ccf0a8a1ced4f59bb62e33ca2
Kimberly Aviles <01010110.v@gmail.com>
2018-10-29 03:41:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,10 +20,13 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
+
+# I'm using .match data again to determine truthiness
+# but only because I'm assuming it's faster than using .scan
 def first_word_capitalized_and_ends_with_punctuation?(text)
   text.match(/^[A-Z].*\.$/) != nil
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(\d{10}|\d{3} \d{3} \d{4}|\(\d{3}\)\d{3}\-\d{4}|\(\d{3}\)\d{7}) != nil
 end
