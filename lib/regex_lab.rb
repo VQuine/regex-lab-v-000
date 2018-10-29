ea5940c2e178adc691929593c1308ca7912c1145
Kimberly Aviles <01010110.v@gmail.com>
2018-10-29 01:51:19 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -4,7 +4,7 @@ end
 
 def words_starting_with_un_and_ending_with_ing(text)
   #/^un\w*ing$/.grep(text)
-  text.count(/^un\w*ing$/)
+  text.grep(/^un\w*ing$/)
 end
 
 def words_five_letters_long(text)
